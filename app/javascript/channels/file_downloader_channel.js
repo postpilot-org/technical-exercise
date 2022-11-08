85f9dcfdb41cf56e428b30e0dec68909c8c3ad4b
Toray Altas <toray.altas@gmail.com>
2022-11-08 15:07:49 -0500

    Uncomment firefox code for downloading file from blob

--- app/javascript/channels/file_downloader_channel.js
+++ app/javascript/channels/file_downloader_channel.js
@@ -20,18 +20,18 @@ consumer.subscriptions.create("FileDownloaderChannel", {
     link.click();
 
     // this is necessary as link.click() does not work on the latest firefox
-    // link.dispatchEvent(
-    //   new MouseEvent('click', { 
-    //     bubbles: true, 
-    //     cancelable: true, 
-    //     view: window 
-    //   })
-    // );
+    link.dispatchEvent(
+      new MouseEvent('click', { 
+        bubbles: true, 
+        cancelable: true, 
+        view: window 
+      })
+    );
 
-    // setTimeout(() => {
-    //   // For Firefox it is necessary to delay revoking the ObjectURL
-    //   window.URL.revokeObjectURL(data);
-    //   link.remove();
-    // }, 100);
+    setTimeout(() => {
+      // For Firefox it is necessary to delay revoking the ObjectURL
+      window.URL.revokeObjectURL(data);
+      link.remove();
+    }, 100);
   }
 });
