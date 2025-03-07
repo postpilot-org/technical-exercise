09bc177229513e1482da31112dcaec6564da78d1
Andre <cavallari@live.com>
2025-03-07 16:14:56 -0300

    Create discount methods for generate status and purge file

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,16 +2,30 @@
 #
 # Table name: discounts
 #
-#  id         :bigint           not null, primary key
-#  kind       :string           default("uploaded")
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :bigint           not null, primary key
+#  csv_export_finished_at :datetime
+#  csv_export_started_at  :datetime
+#  kind                   :string           default("uploaded")
+#  name                   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
 #
 class Discount < ApplicationRecord
   has_many :codes
+  has_one_attached :exported_csv
 
   validates :name, uniqueness: true, presence: true
 
   enum kind: {uploaded: "uploaded"}
+
+  def generating_csv?
+    csv_export_started_at.present? && csv_export_finished_at.blank?
+  end
+
+  def purge_csv!
+    ActiveRecord::Base.transaction do
+      exported_csv.purge
+      update(csv_export_started_at: nil, csv_export_finished_at: nil)
+    end
+  end
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -5,4 +5,29 @@ RSpec.describe Discount, type: :model do
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
+
+  describe '#generating_csv?' do
+    subject { discount.generating_csv? }
+
+    let(:discount) { create(:discount, csv_export_started_at: start_date, csv_export_finished_at: end_date) }
+    let(:start_date) { nil }
+    let(:end_date) { nil }
+
+    context 'when csv_export_started_at and csv_export_finished_at are blank' do
+      it { is_expected.to eq(false) }
+    end
+
+    context 'when csv_export_started_at is present and csv_export_finished_at is blank' do
+      let(:start_date) { Time.zone.now }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'when csv_export_started_at is blank and csv_export_finished_at is present' do
+      let(:start_date) { Time.zone.now }
+      let(:end_date) { Time.zone.now }
+
+      it { is_expected.to eq(false) }
+    end
+  end
 end
