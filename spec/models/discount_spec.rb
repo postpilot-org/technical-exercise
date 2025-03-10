e56b723e5646611f322fd1593b4969a189914eb0
Andre <cavallari@live.com>
2025-03-10 12:10:09 -0300

    Improve spec

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -9,23 +9,22 @@ RSpec.describe Discount, type: :model do
   describe '#generating_csv?' do
     subject { discount.generating_csv? }
 
-    let(:discount) { create(:discount, csv_export_started_at: start_date, csv_export_finished_at: end_date) }
-    let(:start_date) { nil }
-    let(:end_date) { nil }
-
     context 'when csv_export_started_at and csv_export_finished_at are blank' do
+      let(:discount) { create(:discount) }
+
       it { is_expected.to eq(false) }
     end
 
     context 'when csv_export_started_at is present and csv_export_finished_at is blank' do
-      let(:start_date) { Time.zone.now }
+      let(:discount) { create(:discount, csv_export_started_at: Time.zone.now) }
 
       it { is_expected.to eq(true) }
     end
 
-    context 'when csv_export_started_at is blank and csv_export_finished_at is present' do
-      let(:start_date) { Time.zone.now }
-      let(:end_date) { Time.zone.now }
+    context 'when csv_export_started_at and csv_export_finished_at are present' do
+      let(:discount) do
+        create(:discount, csv_export_started_at: Time.zone.now, csv_export_finished_at: Time.zone.now)
+      end
 
       it { is_expected.to eq(false) }
     end
